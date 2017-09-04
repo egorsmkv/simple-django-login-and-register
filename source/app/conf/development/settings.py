3ba192cf1636051623dbcc265ddcf9d65bed68ad
Yehor Smoliakov <egorsmkv@gmail.com>
2017-09-04 18:42:31 +0300

    Added handle for deprecated warnings

--- source/app/conf/development/settings.py
+++ source/app/conf/development/settings.py
@@ -1,5 +1,9 @@
 import os
+import warnings
+
 from os.path import dirname
+
+warnings.simplefilter('error', DeprecationWarning)
 
 BASE_DIR = dirname(dirname(dirname(dirname(os.path.abspath(__file__)))))
 CONTENT_DIR = os.path.join(BASE_DIR, 'content')
