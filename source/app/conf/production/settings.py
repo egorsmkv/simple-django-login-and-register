412a0f1681ccfce6ff1ec6093a47b3e4a2ada7e2
Sunil Kumar Choudari <50225332+Sunilkumarchoudari@users.noreply.github.com>
2022-03-04 12:28:39 +0530

    Update settings.py

--- source/app/conf/production/settings.py
+++ source/app/conf/production/settings.py
@@ -1,6 +1,7 @@
 import os
 from os.path import dirname
-from django.utils.translation import ugettext_lazy as _
+# from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 BASE_DIR = dirname(dirname(dirname(dirname(os.path.abspath(__file__)))))
 CONTENT_DIR = os.path.join(BASE_DIR, 'content')
